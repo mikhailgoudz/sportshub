8dc032c0aa730ba0528f251c01260228a29d2618
Dennis Krupitsky <d.krupitsky@gmail.com>
2019-11-09 12:50:24 -0500

    Changed model to async await

--- app/routes/scores.js
+++ app/routes/scores.js
@@ -2,18 +2,19 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
     controllerName: 'scores',
-    model() {
+    async model() {
         
         const url = 'https://api.sportsdata.io/v3/nfl/scores/json/CurrentWeek?key=c339709091554a78abdf9fa5242fdab9';
         var newHost = this.store.adapterFor('scores').get('host') + '/scores/json/ScoresByWeek/2019REG/';
-        fetch(url).then(response=> {
+       await fetch(url).then(response=> {
             return response.json();
         }).then(data=>{
             console.log(data);
             newHost += `${data}`;
             this.store.adapterFor('scores').set('host', newHost);
-            return this.store.query('scores', {param:' '});
         })
+ 
+        return this.store.query('scores', {param:' '});       
 
 
 
