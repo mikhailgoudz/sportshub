63fbfd909c69cdcb00983601f010e88cbbfb1a0a
Mikhail Goudz <mikhailgoudz@Mikhails-MacBook-Pro.local>
2019-11-10 11:17:43 -0800

    added weekly search method

--- app/routes/scores.js
+++ app/routes/scores.js
@@ -2,6 +2,7 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
     controllerName: 'scores',
+    
     async model() {
         
         const url = 'https://api.sportsdata.io/v3/nfl/scores/json/CurrentWeek?key=c339709091554a78abdf9fa5242fdab9';
@@ -16,8 +17,30 @@ export default Route.extend({
         })
  
         return this.store.query('scores', {param:' '});       
+    
+    },
+
+    actions:{
+
+       async week(){
 
 
+        
 
+            let week = this.controller.get("week");
+           
+           
+            this.store.adapterFor('scores').set('host', 'https://api.sportsdata.io/v3/nfl');
+            var newHost = this.store.adapterFor('scores').get('host') + '/scores/json/ScoresByWeek/2019REG/';
+
+            newHost += `${week}`;
+            this.store.adapterFor('scores').set('host', newHost);
+
+     
+            return this.store.query('scores', {param:' '});       
+        }
     }
 });
+
+
+
