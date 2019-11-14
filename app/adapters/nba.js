d9f6d602ece39a7e3c43896ac99f361e5756575b
Mikhail Goudz <mikhailgoudz@Mikhails-MBP.fios-router.home>
2019-11-13 21:05:56 -0800

    added navbar

--- app/adapters/nba.js
+++ app/adapters/nba.js
@@ -5,7 +5,7 @@ import ENV from 'clutch-sports/config/environment'
 export default DS.JSONAPIAdapter.extend({
   host: 'https://api.sportsdata.io/v3/nba',
   pathForType() {
-    return 'scores/json/GamesByDate/2019-NOV-4'
+    return 'scores/json/GamesByDate/2019-NOV-13'
   },
   buildURL(modelName, id, snapshot, requestType, query = {}) {
     /**
