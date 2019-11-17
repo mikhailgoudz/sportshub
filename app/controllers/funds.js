418a65040a0eb3d3f84a7a9c5c2fca287aaadc77
Mikhail Goudz <mikhailgoudz@Mikhails-MBP.fios-router.home>
2019-11-16 16:15:31 -0800

    working on funds controller

--- app/controllers/funds.js
+++ app/controllers/funds.js
@@ -1,10 +1,32 @@
 import Controller from '@ember/controller';
-
+import { inject as service } from '@ember/service';
 export default Controller.extend({
+
+    session: service(),
+    
     actions: {
-        deposit: function(amount){
-           this.amount = amount;
-           alert(this.amount);
+    
+        deposit(amount){
+
+        this.amount = amount;
+        console.log(amount)
+
+
+        let currentUserId = this.session.data.authenticated.user.uid
+        
+         this.store.query('users', {
+            filter: {
+              uid: currentUserId
+            }
+          }).then(function(user) {
+
+            console.log(user) //expected this to return the model record of which ever user is signed in.. 
+                              // so user.fund should be avaible for us to update the fund on the account
+           
+            console.log(user.fund) //should be 0 but its undefined 
+           
+          })
+           
         }
     }
 });
