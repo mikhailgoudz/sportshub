3333de74fbaaf208a72cfb2a69ca386ad1891772
Dennis Krupitsky <d.krupitsky@gmail.com>
2019-11-22 15:50:29 -0500

    almost done with bets

--- app/controllers/scores.js
+++ app/controllers/scores.js
@@ -13,35 +13,40 @@ export default Controller.extend({
         moneyline(moneyLine,team){   
             
           
-            this.line = moneyLine;
-            this.team = team;
+            this.set("line", moneyLine);
+            this.set("team", team)
             //this.clicked = true;
             this.set("clicked", true);
+
         },
         placeBet(amount) {
-            //   let today = new Date().toString().slice(0,21)
+            let today = new Date().toString().slice(0,21)
             
-            // const newBet = this.store.createRecord('bet',{
+            const newBet = this.store.createRecord('bet',{
                
-            //     team: team,
-            //     odds: moneyLine,
-            //     uid: this.session.data.authenticated.user.uid,
-            //     sport: 'NFL',
-            //     datePlaced: today
+                team: this.team,
+                odds: this.line,
+                uid: this.session.data.authenticated.user.uid,
+                sport: 'NFL',
+                datePlaced: today,
+                wager: amount
                 
          
-            // });
+            });
 
-            // newBet.save();
+            newBet.save();
             
             console.log(this.line);
             console.log(this.team);  
             console.log(amount);
             this.set("clicked", false);
+            
            
         },
         cancelBet: function() {
             this.set("clicked", false);
+            this.set("line", 0);
+            this.set("team", " ")
         }
         
         
