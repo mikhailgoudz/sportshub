649376bd7658e486f1302a652f5260e2ad6f3688
Mikhail Goudz <mikhailgoudz@Mikhails-MacBook-Pro.local>
2019-11-22 11:14:52 -0800

    showing user 's fund on navbar

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,4 +1,11 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default Route.extend({
+
+    session: service(),
+    model(){
+
+        return this.store.query('users',{filter:{uid:this.session.data.authenticated.user.uid}});
+    }
 });
