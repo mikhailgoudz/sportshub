db0949ccdfeb81ddb26bc73dbb87456425f7226d
Mikhail Goudz <mikhailgoudz@Mikhails-MBP.fios-router.home>
2019-11-23 08:25:19 -0800

    divided nba standings to show by their conference

--- app/models/nbastandings.js
+++ app/models/nbastandings.js
@@ -1,4 +1,5 @@
 import DS from 'ember-data';
+import { computed } from '@ember/object';
 const { Model } = DS;
 
 export default Model.extend({
@@ -8,5 +9,25 @@ export default Model.extend({
     Name: DS.attr('string'),
     Wins: DS.attr('string'),
     Losses: DS.attr('string'),
-    Conference: DS.attr('string')
+    Conference: DS.attr('string'),
+    IsEastern: computed('Conference',function(){
+
+        if(this.get('Conference') === 'Eastern'){
+            return true;
+        }
+        else{
+            return false;
+        }
+
+    }),
+    IsWestern: computed('Conference',function(){
+
+        if(this.get('Conference') === 'Western'){
+            return true;
+        }
+        else{
+            return false;
+        }
+
+    })
 });
