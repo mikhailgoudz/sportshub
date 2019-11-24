ee9310e16621af6e72bb6b9f42b8f5bc1ba9aa00
Dennis Krupitsky <d.krupitsky@gmail.com>
2019-11-23 22:33:23 -0500

    manual form validation

--- app/controllers/funds.js
+++ app/controllers/funds.js
@@ -6,9 +6,7 @@ export default Controller.extend({
     
     actions: {
     
-        deposit(amount){
-        
-        
+        deposit(amount, cardnum, expm, expy, cvv){
         this.amount = amount;
         
         let currentUserId = this.session.data.authenticated.user.uid
@@ -19,23 +17,32 @@ export default Controller.extend({
             }
           }).then(function(user) {
             let currentUser = user.firstObject;
-            if(amount != undefined && amount > 0 )
+            if(amount != undefined && amount > 0 &&expm !=undefined && expy != undefined && cvv != undefined && cardnum != undefined)
             {
-              if(currentUser.newUserPromoOptIn && amount >=200){
-              let bonus = 100;
-              currentUser.set("fund" , +amount + +currentUser.fund);
-              currentUser.set("fund", +bonus + +currentUser.fund);
-              currentUser.set("newUser",false);
-              currentUser.set("newUserPromoOptIn",false);
-              currentUser.save();
+              if(cardnum.length < 15 || cardnum.length > 16 || expy.length > 4 || expy.length < 4 || expy < 2019 || expy > 2024 || expm < 1 || expm > 12 || cvv.length <  3 || cvv.length > 4)
+              {
+                alert("PLEASE ENTER VALID CARD");
               }
-              else{
-
-                currentUser.set("fund" , +amount + +currentUser.fund);
-                currentUser.save();
+              else {
+                if(currentUser.newUserPromoOptIn && amount >=200){
+                  let bonus = 100;
+                  currentUser.set("fund" , +amount + +currentUser.fund);
+                  currentUser.set("fund", +bonus + +currentUser.fund);
+                  currentUser.set("newUser",false);
+                  currentUser.set("newUserPromoOptIn",false);
+                  currentUser.save();
+                  }
+                else{
+  
+                  currentUser.set("fund" , +amount + +currentUser.fund);
+                  currentUser.save();
+                }
               }
 
            
+            }
+            else{
+              alert("PLEASE FILL OUT ALL FIELDS");
             }
           });
            
